4d1d04e83275cf1d71ef0f0a67b17ba23e2e5dc4
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-04-02 12:44:34 +0200

    JQUERY 1.2 fixed custom url config

--- helper/jqueryJqueryHelper.php
+++ helper/jqueryJqueryHelper.php
@@ -5,7 +5,7 @@ class jqueryJqueryHelper extends jqueryJqueryHelper_Parent
     public function getUrl()
     {
         if (Clementine::$config['module_jquery']['url']) {
-            return $url; 
+            return Clementine::$config['module_jquery']['url'];
         }
         return __WWW_ROOT_JQUERY__ . '/skin/js/jquery.min.js';
     }
